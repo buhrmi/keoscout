fc8f0472fcbe186b4cc164cc8e8ee709baf04838
Stefan Buhrmester <stefan@buhrmi.de>
2026-06-12 19:47:29 +0700

    move friends invitation into modal

--- Modal.svelte
+++ Modal.svelte
@@ -2,6 +2,7 @@
   import { mount, unmount } from 'svelte';
   import { Frame } from 'inertiax-svelte'
   import Modal from './Modal.svelte';
+  import { cubicOut } from 'svelte/easing';
 
   export function modal(node) {
     node.addEventListener('click', (e) => {
@@ -19,6 +20,20 @@
     })
   }
 
+
+ function variable(node, { delay = 0 }) {
+    // duration on desktop is 300, on mobile 400
+    const duration = window.innerWidth > 768 ? 300 : 400;
+    return {
+      delay,
+      duration,
+      easing: cubicOut,
+      tick: (t) => {
+        node.style.setProperty("--progress", t);
+      },
+      // css: (t) => `--transition: ${t}`
+    };
+  }
 </script>
 
 <script>
@@ -26,10 +41,10 @@
   const { src, close } = $props()
 </script>
 
-<div class="modal_wrapper" transition:fade={{duration: 100}}>
+<div class="modal_wrapper">
   <!-- svelte-ignore a11y_click_events_have_key_events,a11y_no_static_element_interactions -->
-  <div class="modal_bg" onclick={close}></div>
-  <div class="modal layout" aria-modal="true" role="dialog" in:scale={{duration: 200}}>
+  <div class="modal_bg" onclick={close} transition:fade></div>
+  <div class="modal layout" aria-modal="true" role="dialog" transition:variable>
     <Frame {src} />
     <nav>
       <button onclick={close} aria-label="Close modal">
@@ -41,24 +56,16 @@
 
 <style>
 .modal_wrapper {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1000;
-  display: grid;
-  place-items: center;
+
 }
 .modal_bg {
-  background: rgba(0, 0, 0, 0.5);
-  backdrop-filter: blur(4px);
+  background: rgba(0, 0, 0, 0.7);
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  z-index: -1;
+  /* z-index: -1; */
 }
 nav {
   grid-area: header;
@@ -68,6 +75,16 @@ nav {
     font-size: 1.5rem;
     
   }
+}
+.modal {
+  position: fixed;
+  bottom: 0;
+  transform: translateY(calc((1 - var(--progress)) * 100%));
+  background-color: #00000022;
+  border: 1px solid #333;
+  border-top-left-radius: 2rem;
+  border-top-right-radius: 2rem;
+  backdrop-filter: blur(8px);
 }
 
 @media (min-width: 640px) {
